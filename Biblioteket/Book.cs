998bf82adbc7c47e4d5369645c7d910a80a3db47
Jesper Sandell <sandell327@gmail.com>
2021-02-23 08:03:15 +0100

    Add project files.

---
+++ Biblioteket/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteket
+{
+    class Book
+    {
+        private string _Title { get; }
+
+        private string _Auther { get; set; }
+
+
+        public string Title 
+        {
+            get => _Title;
+        }
+
+        public string Auther
+        {
+            get => _Auther;
+        }
+
+
+        public Book(string title, string auther) 
+        {
+            _Title = title;
+            _Auther = auther;
+        }
+
+
+    }
+}
---
+++ Biblioteket/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>();
+            Stack<Book> customer = new Stack<Book>();
+
+            books.Add(new Book("The future of programming", "Nerd"));
+            books.Add(new Book("The future of programming 2", "Nerd"));
+            books.Add(new Book("The future of programming 3", "Nerd"));
+            books.Add(new Book("Gaming", "Gamer"));
+            books.Add(new Book("Gaming 2", "Gamer"));
+            books.Add(new Book("Gaming 3", "Gamer"));
+
+            bool power = true;
+            while (power)
+            {
+
+                Console.Clear();
+                
+
+                foreach (Book book in books)
+                {
+                    Console.WriteLine($"{book.Title}, {book.Auther} ");
+
+                }
+
+                Console.WriteLine("Choice a book: ");
+                string input = Console.ReadLine();
+
+                Book choicingBook = new Book("","");
+                foreach (Book book in books)
+                {
+                    if(book.Title.ToLower() == input.ToLower())
+                    {
+                        choicingBook = book;
+                    }
+
+                }
+
+                customer.Push(choicingBook);
+                books.Remove(choicingBook);
+                Console.WriteLine();
+                Console.WriteLine("Do you want to get more books? N/Y");
+                input = Console.ReadLine();
+
+                if (input.ToLower() == "n" || books.Count == 0) 
+                {
+                    power = false;
+                }
+
+            }
+
+            for (int i = 0; i < customer.Count; i++)
+            {
+                Book deletingBook = customer.Peek();
+
+                Console.WriteLine($"Now getting {deletingBook.Title}.....");
+
+                customer.Pop();
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
